6418bd8ce44f8248c79f72e7b82783c61ad09721
Eichi-Arakaki <justanunusualperson@gmail.com>
2022-01-01 17:15:26 +0900

    main.asm, linker.il created

---
+++ src/impl/x86_64/boot/main.asm
@@ -0,0 +1,12 @@
+; Entry point to operation system
+global start
+
+section .text
+bits 32 ; still in 32-bit mode at this point
+start:
+    ; print "OK"
+    mov dword [0xb8000], 0x2fb42f4f
+    hlt ; instructs to cpu to completely freeze
+
+
+
