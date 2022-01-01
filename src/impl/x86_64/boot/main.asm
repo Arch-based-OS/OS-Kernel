73b21bed9b32b2710cdc1a2160beecb7ec14cf33
Eichi-Arakaki <justanunusualperson@gmail.com>
2022-01-01 19:14:33 +0900

    memory variables fixed

--- src/impl/x86_64/boot/main.asm
+++ src/impl/x86_64/boot/main.asm
@@ -5,7 +5,7 @@ section .text
 bits 32 ; still in 32-bit mode at this point
 start:
     ; print "OK"
-    mov dword [0xb8000], 0x2fb42f4f
+    mov dword [0xb8000], 0x2f4b2f4f
     hlt ; instructs to cpu to completely freeze
 
 
